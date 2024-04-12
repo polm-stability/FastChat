757f2135cdb8b5c203a04ef9f0d040b8e09da576
Paul McCann <paul.mccann@stability.ai>
2024-04-12 14:04:04 +0000

    Open json with utf-8 encoding
    Without this, it is possible to get a UnicodeDecodeError because the
    file is opened as ascii.
    
    I am not sure why the file is opened as ascii in Python 3. I am also
    unsure why it would happen now if it hadn't happened before. However,
    whatever the cause, being explicit fixes it.

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -86,7 +86,7 @@ class MatchPair:
 def load_questions(question_file: str, begin: Optional[int], end: Optional[int]):
     """Load questions from a file."""
     questions = []
-    with open(question_file, "r") as ques_file:
+    with open(question_file, "r", encoding="utf-8") as ques_file:
         for line in ques_file:
             if line:
                 questions.append(json.loads(line))
